cb2233c0ca5b7d8d1b508614ece84c88d79c745d
kalada.kienka <kalada.kienka@konga.com>
2016-10-25 21:50:54 +0100

    convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -37,15 +37,16 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
-
-    res.send('Asking for todo with id ' + req.params.id);
+    db.todo.findById(todoId).then(function (todo) {
+        if (!!todo) {
+            res.json(todo.toJSON());
+        } else {
+            res.status(404).send();
+        }
+    }, function (e) {
+        res.status(500).send();
+    });
 });
 
 // POST /todos
@@ -57,16 +58,6 @@ app.post('/todos', function (req, res) {
     }, function (e) {
         res.status(400).json(e);
     });
-
-    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    //     return res.status(400).send();
-    // }
-    //
-    // body.description = body.description.trim();
-    // body.id = todoNextId++;
-    // todos.push(body);
-    //
-    // res.json(body);
 });
 
 // PUT /todos/:id
